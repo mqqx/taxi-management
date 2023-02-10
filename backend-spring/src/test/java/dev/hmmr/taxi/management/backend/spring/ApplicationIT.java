def97712edbc20b2be13f562e87a4f46137d5058
mqqx <taxi-management@hmmr.dev>
2023-02-10 21:13:05 +0700

    ✨ Add Feature * Initial setup of `backend-spring` project which will be used as one of multiple backend example implementations

---
+++ backend-spring/src/test/java/dev/hmmr/taxi/management/backend/spring/ApplicationIT.java
@@ -0,0 +1,11 @@
+package dev.hmmr.taxi.management.backend.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationIT {
+
+  @Test
+  void contextLoads() {}
+}
