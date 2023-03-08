e65a906d7d1e0c6e51511295e3de22b3c80b2fff
mqqx <taxi-management@hmmr.dev>
2023-03-08 13:22:04 +0700

    ✨ Add Feature - Cors Support * Add support for cors in `backend-spring` app to allow calls from angular app by default * URL set for local development, needs to be overridden in hosted environments

---
+++ backend-spring/src/main/java/dev/hmmr/taxi/management/backend/spring/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package dev.hmmr.taxi.management.backend.spring.config;
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class CorsConfig {
+  @Value("${taxi-management.allowed-origins}")
+  String allowedOrigins;
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins(allowedOrigins);
+      }
+    };
+  }
+}
