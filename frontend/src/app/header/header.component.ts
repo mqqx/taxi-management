8fc88f4a34da19353d2e0c56a7f20603ff036a01
mqqx <taxi-management@hmmr.dev>
2023-03-08 17:24:06 +0700

    🛠 Refactoring Code - Remove Obsolete OnInit * Remove obsolete OnInit

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,16 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'tm-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
   constructor() {
   }
-
-  ngOnInit(): void {
-  }
-
 }
