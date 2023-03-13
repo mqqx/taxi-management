b993a7dff181a739883c523d512c332d860f03a0
mqqx <taxi-management@hmmr.dev>
2023-03-13 15:45:40 +0700

    🐞 Fix Bug - CORS-Config-Methods * Add HTTP-Methods to CORS config to ensure not only `GET` calls, but all other methods as well are allowed to be called from defined frontend URI

--- backend-spring/src/main/java/dev/hmmr/taxi/management/backend/spring/config/CorsConfig.java
+++ backend-spring/src/main/java/dev/hmmr/taxi/management/backend/spring/config/CorsConfig.java
@@ -1,5 +1,13 @@
 package dev.hmmr.taxi.management.backend.spring.config;
 
+import static org.springframework.http.HttpMethod.DELETE;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.HEAD;
+import static org.springframework.http.HttpMethod.OPTIONS;
+import static org.springframework.http.HttpMethod.PATCH;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpMethod.PUT;
+
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,7 +27,17 @@ public class CorsConfig {
     return new WebMvcConfigurer() {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins(allowedOrigins);
+        registry
+            .addMapping("/**")
+            .allowedMethods(
+                HEAD.name(),
+                GET.name(),
+                POST.name(),
+                PUT.name(),
+                DELETE.name(),
+                PATCH.name(),
+                OPTIONS.name())
+            .allowedOrigins(allowedOrigins);
       }
     };
   }
